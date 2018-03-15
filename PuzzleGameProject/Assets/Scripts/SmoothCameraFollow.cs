d7b0637aa6d8af349d76859b740083d8507d5ef9
Jack <c15729315@mydit.ie>
2018-03-15 20:01:09 +0000

    Bits of the level done. Smooth camera follow

---
+++ PuzzleGameProject/Assets/Scripts/SmoothCameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothCameraFollow : MonoBehaviour
+{
+    public float smoothTime;
+    [Space(10)]
+    public Transform target;
+    private Vector3 velocity = Vector3.zero;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPosition = target.TransformPoint(new Vector3(0, 0, -10));
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    }
+}

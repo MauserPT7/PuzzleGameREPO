0eb5ab2067d891a124ceddcd3dc7907724fa18e7
Jack <c15729315@mydit.ie>
2018-03-16 11:03:12 +0000

    Jumping and Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour 
 {
-	// Use this for initialization
-	void Start () 
+    public float moveSpeed = 0.0f;
+    public float maxMoveSpeed = 5.0f;
+    public float jumpForce = 10.0f;
+
+    public bool isGrounded = false;
+
+    Rigidbody2D myRB2D;
+
+    public GameObject checkpoint1;
+
+    // Use this for initialization
+    void Start () 
 	{
-		
+        myRB2D = this.gameObject.GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-		
+		if(transform.position.y <= -10)
+        {
+            transform.position = checkpoint1.transform.position;
+        }
 	}
+
+    // Use for physics calculations (rigidbodies)
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        moveSpeed = maxMoveSpeed;
+
+        myRB2D.velocity = new Vector2(moveHorizontal * moveSpeed, myRB2D.velocity.y);
+        
+        if(myRB2D.velocity.x == 0
+           && isGrounded)
+        {
+            myRB2D.gravityScale = 0;
+        } else {
+            myRB2D.gravityScale = 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)
+            && isGrounded)
+        {
+            Debug.Log(Time.time);
+            myRB2D.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "World1" || collision.gameObject.tag == "World2")
+        {
+            isGrounded = true;
+        }
+    }
 }

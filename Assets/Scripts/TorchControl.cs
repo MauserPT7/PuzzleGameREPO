62436c140ee26b8ad8be03ec8e71f29bc551a3df
NickMcG-PC <D11123772@mydit.ie>
2018-03-19 13:25:32 +0000

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/TorchControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		AimTorch();
+	}
+
+	void AimTorch()
+	{
+		Vector3 mousePosition;
+
+		mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Quaternion myRotation = Quaternion.LookRotation(transform.position - mousePosition,Vector3.forward);
+		transform.localRotation = myRotation;
+		transform.eulerAngles = new Vector3(0,0,transform.eulerAngles.z);
+		
+	}
+}
